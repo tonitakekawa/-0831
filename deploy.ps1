1f05ddec9cbea2f01bce2715c5ee656f2e2f5f3d
tonitakekawa <tonitakekawa@gmail.com>
2026-05-12 23:23:28 +0900

    .

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,13 @@
-pandoc '内容.md' -o index.html --standalone --include-after-body=action.html --variable title='' --metadata title='ἀπορία'
-if ($?) { git add .; git commit -m '.'; git push }
+pandoc top.md 出来事.md 好き.md 内容.md `
+  -o index.html `
+  --standalone `
+  --include-in-header=frame-header.html `
+  --include-before-body=frame-before.html `
+  --include-after-body=action.html `
+  --variable title="" `
+  --metadata title="ἀπορία"
+
+git add .
+$staged = git diff --cached --name-only
+if ($staged) { git commit -m '.' }
+git push
